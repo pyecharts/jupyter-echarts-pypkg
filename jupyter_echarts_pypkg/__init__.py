9f5831e28417116bfbd057ca8c05053fa8366594
chfw <wangc_2011@hotmail.com>
2018-02-08 19:13:10 +0000

    :sparkles: initial implementation of jupyter-echarts as python package

--- jupyter_echarts_pypkg/__init__.py
+++ jupyter_echarts_pypkg/__init__.py
@@ -1,3 +1,17 @@
 # flake8: noqa
+import os
+
 from jupyter_echarts_pypkg._version import __version__
 from jupyter_echarts_pypkg._version import __author__
+from lml.plugin import PluginInfoChain, PluginInfo
+from pyecharts.js_extension import JsExtension
+
+
+@PluginInfo('pyecharts_js_extension', 'all')
+class JupyterEcharts(JsExtension):
+    def __init__(self):
+        __package_path__ = os.path.dirname(__file__)
+        __registry_json__ = os.path.join(
+            __package_path__, "resources")
+        super(JupyterEcharts, self).__init__(__registry_json__)
+
