ca6bc836438cbbdf7602c3cc193594278b5f0408
chfw <wangc_2011@hotmail.com>
2018-02-12 07:56:09 +0000

    :fire: break the dependency on pyecharts

--- jupyter_echarts_pypkg/__init__.py
+++ jupyter_echarts_pypkg/__init__.py
@@ -4,13 +4,12 @@ import os
 from jupyter_echarts_pypkg._version import __version__
 from jupyter_echarts_pypkg._version import __author__
 from lml.plugin import PluginInfoChain, PluginInfo
-from pyecharts.js_extensions import JsExtension
 
 
 @PluginInfo('pyecharts_js_extension', tags=['core'])
 class Pypkg():
     def __init__(self):
         __package_path__ = os.path.dirname(__file__)
-        __registry_json__ = os.path.join(
+        self.js_extension_path = os.path.join(
             __package_path__, "resources")
-        self.js_extension = JsExtension.from_registry_path(__registry_json__)
+
